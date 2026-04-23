84c0701f972ca2cc61b46f6e45159b1b3c657bb4
Orchid42 <zeemorchid@gmail.com>
2026-04-24 02:55:07 +1200

    pdf18

--- UnicornShooter3/Assets/Scripts/SlidingDoor.cs
+++ UnicornShooter3/Assets/Scripts/SlidingDoor.cs
@@ -2,15 +2,58 @@ using UnityEngine;
 
 public class SlidingDoor : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform leftDoor;
+    public Transform rightDoor;
+
+    public float openDistance = 1f;
+    public float openSpeed = 2f;
+
+    private Vector3 leftClosedPosition;
+    private Vector3 rightClosedPosition;
+    private Vector3 leftOpenPosition;
+    private Vector3 rightOpenPosition;
+
+    private bool isOpening = false;
+    private bool isClosing = false;
+
+
     void Start()
     {
-        
+        leftClosedPosition = leftDoor.localPosition;
+        rightClosedPosition = rightDoor.localPosition;
+
+        leftOpenPosition = leftClosedPosition + Vector3.left * openDistance;
+        rightOpenPosition = rightClosedPosition + Vector3.right * openDistance;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isOpening)
+        {
+            leftDoor.localPosition = Vector3.Lerp(leftDoor.localPosition, leftOpenPosition, openSpeed * Time.deltaTime);
+            rightDoor.localPosition = Vector3.Lerp(rightDoor.localPosition, rightOpenPosition, openSpeed * Time.deltaTime);
+        }
+        else if (isClosing)
+        {
+            leftDoor.localPosition = Vector3.Lerp(leftDoor.localPosition, leftClosedPosition, openSpeed * Time.deltaTime);
+            rightDoor.localPosition = Vector3.Lerp(rightDoor.localPosition, rightClosedPosition, openSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isOpening = true;
+            isClosing = false;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isOpening = false;
+            isClosing = true;
+        }
     }
 }
